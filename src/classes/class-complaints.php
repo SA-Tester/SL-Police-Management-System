f44f37f248a383a2955f0bb038a990ea09f3ce57
@SIA <59891156+SA-Tester@users.noreply.github.com>
2023-07-23 01:53:43 +0530

    Fixing classes in the project and adding their functionalities

--- src/classes/class-complaints.php
+++ src/classes/class-complaints.php
@@ -1,35 +1,18 @@
 <?php 
 
-namespace Complaints;
-
-use DBConnector\DBConnector;
+namespace classes;
 use PDOException;
-use PDO;
 
-include "./interface-complaints.php";
-include "./class-db-connector.php";
-
-class ComplaintManager implements Complaints{
+class Complaints{
+    private $complaint_id;
     private $date;
     private $category;
     private $title;
     private $recording;
     private $description;
-    private $plantiff_name;
-    private $plantiff_nic;
-    private $plantiff_address;
-    private $plantiff_contact;
-    private $plantiff_email;
     private $complaint_status;
     private $emp_id;
-
-    private $vehicle_number;
-    private $temp_license_start;
-    private $temp_license_end;
-    private $fine_amount;
-    private $city;
-    private $latitude;
-    private $longitude;
+    private $con;
 
     // SETTERS 
     function setDate($date){
@@ -52,26 +35,6 @@ class ComplaintManager implements Complaints{
         $this->description = $description;
     }
 
-    function setPlantiffName($plantiff_name){
-        $this->plantiff_name = $plantiff_name; 
-    }
-
-    function setPlantiffNIC($plantiff_nic){
-        $this->plantiff_nic = $plantiff_nic;
-    }
-
-    function setPlantiffAddress($plantiff_address){
-        $this->plantiff_address = $plantiff_address;
-    }
-
-    function setPlantiffContact($plantiff_contact){
-        $this->plantiff_contact = $plantiff_contact;
-    }
-
-    function setPlantiffEmail($plantiff_email){
-        $this->plantiff_email = $plantiff_email;
-    }
-
     function setComplaintStatus($complaint_status){
         $this->complaint_status = $complaint_status;
     }
@@ -80,113 +43,15 @@ class ComplaintManager implements Complaints{
         $this->emp_id = $emp_id;
     }
 
-    function setVehicleNumber($vehicle_number){
-        $this->vehicle_number = $vehicle_number;
+    function setCon($con){
+        $this->con = $con;
     }
 
-    function setTempLicenseStart($temp_license_start){
-        $this->temp_license_start = $temp_license_start;
+    function getComplaintID(){
+        return $this->complaint_id;
     }
 
-    function setTempLicenseEnd($temp_license_end){
-        $this->temp_license_end = $temp_license_end;
-    }
-
-    function setFineAmount($fine_amount){
-        $this->fine_amount = $fine_amount;
-    }
-
-    function setCity($city){
-        $this->city = $city;
-    }
-
-    function setLatitude($latitude){
-        $this->latitude = $latitude;
-    }
-
-    function setLongitude($longitude){
-        $this->longitude = $longitude;
-    }
-
-    // GETTERS
-    function getDate(){
-        return $this->date;
-    }
-
-    function getCategory(){
-        return $this->category;
-    }
-
-    function getTitle(){
-        return $this->title;
-    }
-
-    function getRecording(){
-        return $this->recording;
-    }
-
-    function getDescription(){
-        return $this->description;
-    }
-
-    function getPlantiffName(){
-        return $this->plantiff_name;
-    }
-
-    function getPlantiffNIC(){
-        return $this->plantiff_nic;
-    }
-
-    function getPlantiffAddress(){
-        return $this->plantiff_address;
-    }
-
-    function getPlantiffContact(){
-        return $this->plantiff_contact;
-    }
-
-    function getPlantiffEmail(){
-        return $this->plantiff_email;
-    }
-
-    function getComplaintStatus(){
-        return $this->complaint_status;
-    }
-
-    function getEmpID(){
-        return $this->emp_id;
-    }
-
-    function getVehicleNumber(){
-        return $this->vehicle_number;
-    }
-
-    function getTempLicenseStart(){
-        return $this->temp_license_start;
-    }
-
-    function getTempLicenseEnd(){
-        return $this->temp_license_end;
-    }
-
-    function getFineAmount(){
-        return $this->fine_amount;
-    }
-
-    function getCity(){
-        return $this->city;
-    }
-
-    function getLatitude(){
-        return $this->latitude;
-    }
-
-    function getLongitude(){
-        return $this->longitude;
-    }
-
-    function convertCategory($category)
-    {
+    function convertCategory($category){
         switch($category){
             case "1":
                 return "Abuse of Women or Children";
@@ -314,109 +179,67 @@ class ComplaintManager implements Complaints{
         }
     }
 
-    function saveAudio()
-    {
+    function saveAudio(){
         $url =  $this->recording;
-        //echo $url;
         // ADD CODE TO UPLOAD THE FILE TO UPLOADS FOLDER
+        //should return uploads/recordings/FILENAME.mp3
     }
 
-    function addComplaint($type){
-        $dbcon = new DBConnector();
-        $con = $dbcon->getConnection();
-        
-        $query1 = "INSERT INTO location(location_name, district, city, latitude, longitude) VALUES(?, ?, ?, ?, ?)";
-        $query2 = "INSERT INTO people(nic, name, address, contact, email) VALUES(?, ?, ?, ?, ?)";
-        $query3 = "INSERT INTO complaint(date, complaint_type, complaint_title, audio_src, complaint_text, complaint_status, empID) VALUES(?, ?, ?, ?, ?, ?, ?)";
-        $query4 = "INSERT INTO complaint(date, complaint_type, complaint_title, audio_src, complaint_text, complaint_status, empID, location_id) VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
-        $query5 = "INSERT INTO role_in_case(nic, role_in_case, complaint_id) VALUES(?, ?, ?)";
-        $query6 = "INSERT INTO fine(complaint_id, nic, vehicle_number, temp_license_start_date, temp_license_end_date, fine_amount, fine_status, license_issued) VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
-        
-        switch($type){
-            case "1":
-                $pstmt1 = $con->prepare($query2);
-                $pstmt1->bindValue(1, $this->plantiff_nic);
-                $pstmt1->bindValue(2, $this->plantiff_name);
-                $pstmt1->bindValue(3, $this->plantiff_address);
-                $pstmt1->bindValue(4, $this->plantiff_contact);
-                $pstmt1->bindValue(5, $this->plantiff_email);
-                try{
-                    $a = $pstmt1->execute();
-                    if($a > 0){
-                        echo "Record added to the people table successfully";
-                    }
-                    else{
-                        echo "An Error occured";
-                    }
-                }catch(PDOException $e){
-                    $e->getMessage();
-                }
+    public function addComplaint($location_id){
+        if($location_id == ""){
+            $query = "INSERT INTO complaint(date, complaint_type, complaint_title, audio_src, complaint_text, complaint_status, empID) VALUES(?, ?, ?, ?, ?, ?, ?)";
+            try{
+                $pstmt = $this->con->prepare($query);
+                
+                $pstmt->bindValue(1, $this->date);
+                $pstmt->bindValue(2, $this->category);
+                $pstmt->bindValue(3, $this->title);
+                $pstmt->bindValue(4, $this->recording);
+                $pstmt->bindValue(5, $this->description);
+                $pstmt->bindValue(6, $this->complaint_status);
+                $pstmt->bindValue(7, $this->emp_id);
 
-                $complaint_id = "";
-                $pstmt2 = $con->prepare($query3);
-                $pstmt2->bindValue(1, $this->date);
-                $pstmt2->bindValue(2, $this->category);
-                $pstmt2->bindValue(3, $this->title);
-                $pstmt2->bindValue(4, $this->recording);
-                $pstmt2->bindValue(5, $this->description);
-                $pstmt2->bindValue(6, $this->complaint_status);
-                $pstmt2->bindValue(7, $this->emp_id);
-                try{
-                    $b = $pstmt2->execute();
-                    $complaint_id = $con->lastInsertId();;
-                    if($b > 0){
-                        echo "Record added to complaint table successfully";
-                    }
-                    else{
-                        echo "An Error occured";
-                    }
-                }catch(PDOException $e){
-                    $e->getMessage();
+                $a = $pstmt->execute();
+                $this->complaint_id = $this->con->lastInsertId();
+                
+                if($a > 0){
+                    echo "Record added to complaint table successfully<br>";
                 }
-
-                $pstmt3 = $con->prepare($query5);
-                $pstmt3->bindValue(1, $this->plantiff_nic);
-                $pstmt3->bindValue(2, "Plantiff");
-                $pstmt3->bindValue(3, $complaint_id);
-                try{
-                    $c = $pstmt3->execute();
-                    if($c > 0){
-                        echo "Record added to role in case table successfully";
-                    }
-                    else{
-                        echo "An Error occured";
-                    }
-                }catch(PDOException $e){
-                    $e->getMessage();
+                else{
+                    die("An Error occured<br>");
                 }
-
-                break;
+            }
+            catch(PDOException $e){
+                echo $e->getMessage();
+            }
         }
+        else{
 
-        /*//if location is NULL
-        $complaint_id = "SELECT last_insert_id()";
-        
-        //if location is not null
-        $query1 = "INSERT INTO location(location_name, district, city, latitude, longitude) VALUES(?, ?, ?, ?, ?)";
-        $location_id = "SELECT last_insert_id()";
-        $query2 = "INSERT INTO people(nic, name, address, contact, email) VALUES(?, ?, ?, ?, ?)";
-        $query3 = "INSERT INTO complaint(date, complaint_type, complaint_title, audio_src, complaint_text, complaint_status, empID, location_id) VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
-        $complaint_id = "SELECT last_insert_id()";
-        $query4 = "INSERT INTO role_in_case(nic, role_in_case, complaint_id) VALUES(?, ?, ?)";
-
-        // if complaint = traffic
-        $query1 = "INSERT INTO location(location_name, district, city, latitude, longitude) VALUES(?, ?, ?, ?, ?)";
-        $query2 = "INSERT INTO people(nic, name, address, contact, email) VALUES(?, ?, ?, ?, ?)";
-        $location_id = "SELECT last_insert_id()";
-        $query3 = "INSERT INTO complaint(date, complaint_type, complaint_title, audio_src, complaint_text, complaint_status, empID, location_id) VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
-        $complaint_id = "SELECT last_insert_id()";
-        $query4 = "INSERT INTO role_in_case(nic, role_in_case, complaint_id) VALUES(?, ?, ?)";
-        $query4 = "INSERT INTO fine(complaint_id, nic, vehicle_number, temp_license_start_date, temp_license_end_date, fine_amount, fine_status, license_issued) VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
-        */
+        }
     }
 
-    function updateComplaint()
-    {
+    function addRoleInCase($plantiff_nic){
+        $query = "INSERT INTO role_in_case(nic, role_in_case, complaint_id) VALUES(?, ?, ?)";
+
+        try{
+            $pstmt = $this->con->prepare($query);
+            $pstmt->bindValue(1, $plantiff_nic);
+            $pstmt->bindValue(2, "Plantiff");
+            $pstmt->bindValue(3, $this->complaint_id);
+
+            $a = $pstmt->execute();
+            if($a > 0){
+                echo "Record added to role in case table successfully<br>";
+            }
+            else{
+                die("An Error occured");
+            }
+        }catch(PDOException $e){
+            $e->getMessage();
+        }
+    }
+
+    function updateComplaint(){
         
     }
 }
---
+++ src/classes/class-location.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace classes;
+
+use PDOException;
+
+class Location{
+    private $location_id;
+    private $name;
+    private $district;
+    private $city;
+    private $latitude;
+    private $longitude;
+    private $con;
+
+    public function __construct($name, $district, $city, $latitude, $longitude){
+        $this->name = $name;
+        $this->district = $district;
+        $this->city = $city;
+        $this->latitude = $latitude;
+        $this->longitude = $longitude;
+    }
+
+    public function setCon($con){
+        $this->con = $con;
+    }
+
+    public function getLocationID(){
+        return $this->location_id;
+    }
+
+    public function addLocation(){
+        $query = "INSERT INTO location(location_name, district, city, latitude, longitude) VALUES(?, ?, ?, ?, ?)";
+        try{
+            $pstmt = $this->con->prepare($query);
+            $pstmt->bindValue(1, $this->name);
+            $pstmt->bindValue(2, $this->district);
+            $pstmt->bindValue(3, $this->city);
+            $pstmt->bindValue(4, $this->latitude);
+            $pstmt->bindValue(5, $this->longitude);
+            
+            $a = $pstmt->execute();
+            $this->location_id = $this->con->lastInsertId();
+
+            if($a>0){
+                echo "Location: SUCCESS<br>";
+            }
+            else{
+                die("An error occured");
+            }
+        }
+        catch(PDOException $e){
+            $e->getMessage();
+        }
+    }
+}
