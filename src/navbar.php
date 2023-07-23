426f3420a70a6612ef6f69a0e47f725a5133204e
@SIA <59891156+SA-Tester@users.noreply.github.com>
2023-07-23 14:09:48 +0530

    Updating links in Navbar

--- src/navbar.php
+++ src/navbar.php
@@ -10,7 +10,7 @@ function renderNavBar()
             <div class="row">
               <div class="col-md-12">
                   <nav class="navbar navbar-expand-lg navbar-light fixed-top bg-white m-0 mt-0 mb-0">
-                    <a class="navbar-brand" href="index.html">
+                    <a class="navbar-brand" href="index.php">
                       <div class="d-flex align-items-center">
                         <img src="../assets/logo.png" width="70" height="70" alt="logo">
                         <h3 class="d-none d-lg-inline-block align-top mt-3 h3" style="color:#101D6B">Sri Lanka Police</h3>
@@ -24,7 +24,7 @@ function renderNavBar()
                     <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
                       <ul class="navbar-nav">
                         <li class="nav-item">
-                          <a class="nav-link" href="index.html" style="color:#101D6B">Home<span class="sr-only">(current)</span></a>
+                          <a class="nav-link" href="index.php" style="color:#101D6B">Home<span class="sr-only">(current)</span></a>
                         </li>
                         <li class="nav-item dropdown">
                           <a class="nav-link dropdown-toggle" href="#" id="complaintsDropdown" role="button"
@@ -33,36 +33,36 @@ function renderNavBar()
                           </a>
                           <div class="dropdown-menu" aria-labelledby="complaintsDropdown">
                             <a class="dropdown-item" href="record-complaints.php" style="color:#101D6B"><b>Add/View Complaints</b></a>
-                            <a class="dropdown-item" href="view-people.html" style="color:#101D6B"><b>View People and Cases</b></a>
-                            <a class="dropdown-item" href="complaint-study.html" style="color:#101D6B"><b>Complaint Study</b></a>
+                            <a class="dropdown-item" href="view-people.php" style="color:#101D6B"><b>View People and Cases</b></a>
+                            <a class="dropdown-item" href="complaint-study.php" style="color:#101D6B"><b>Complaint Study</b></a>
                           </div>
                         </li>
                         <li class="nav-item dropdown">
                           <a class="nav-link dropdown-toggle" href="#" id="dutiesDropdown" role="button"
                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color:#101D6B">Duties</a>
                           <div class="dropdown-menu" aria-labelledby="dutiesDropdown">
-                            <a class="dropdown-item" href="general_Duty_Update.html" style="color:#101D6B"><b>General Duty</b></a>
-                            <a class="dropdown-item" href="special-duty.html" style="color:#101D6B"><b>Special Duty</b></a>
-                            <a class="dropdown-item" href="emergency-duties.html" style="color:#101D6B"><b>Emergency Duty</b></a>
+                            <a class="dropdown-item" href="general-duty.php" style="color:#101D6B"><b>General Duty</b></a>
+                            <a class="dropdown-item" href="special-duty.php" style="color:#101D6B"><b>Special Duty</b></a>
+                            <a class="dropdown-item" href="emergency-duties.php" style="color:#101D6B"><b>Emergency Duty</b></a>
                           </div>
                         </li>
                         <li class="nav-item dropdown">
                           <a class="nav-link dropdown-toggle" href="#" id="leavesDropdown" role="button"
                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color:#101D6B">Leaves</a>
                           <div class="dropdown-menu" aria-labelledby="leavesDropdown">
-                            <a class="dropdown-item" href="submit-leave-medical.html" style="color:#101D6B"><b>Submit Leaves</b></a>
+                            <a class="dropdown-item" href="submit-leave-medical.php" style="color:#101D6B"><b>Submit Leaves</b></a>
                           </div>
                         </li>
                         <li class="nav-item dropdown">
                           <a class="nav-link dropdown-toggle" href="#" id="employeeDropdown" role="button"
                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color:#101D6B">Employee</a>
                           <div class="dropdown-menu" aria-labelledby="employeeDropdown">
-                            <a class="dropdown-item" href="New_Employee.html" style="color:#101D6B"><b>Add New Employee</b></a>
-                            <a class="dropdown-item" href="check-avalability.html" style="color:#101D6B"><b>Check Employee Avalability</b></a>
+                            <a class="dropdown-item" href="new-employee.php" style="color:#101D6B"><b>Add New Employee</b></a>
+                            <a class="dropdown-item" href="check-avalability.php" style="color:#101D6B"><b>Check Employee Avalability</b></a>
                           </div>
                         </li>
                         <li class="nav-item">
-                          <a class="nav-link" href="payroll.html" style="color:#101D6B">Salary</a>
+                          <a class="nav-link" href="payroll.php" style="color:#101D6B">Salary</a>
                         </li>
                         <li class="nav-item">
                           <a class="nav-link" href="#" style="color:#101D6B;">Settings</a>
@@ -80,7 +80,7 @@ function renderNavBar()
             <div class="row">
               <div class="col-md-12">
                   <nav class="navbar navbar-expand-lg navbar-light fixed-top bg-white m-0 mt-0 mb-0">
-                    <a class="navbar-brand" href="index.html">
+                    <a class="navbar-brand" href="index.php">
                       <div class="d-flex align-items-center">
                         <img src="../assets/logo.png" width="70" height="70" alt="logo">
                         <h3 class="d-none d-lg-inline-block align-top mt-3 h3" style="color:#101D6B">Sri Lanka Police</h3>
@@ -94,7 +94,7 @@ function renderNavBar()
                     <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
                       <ul class="navbar-nav">
                         <li class="nav-item">
-                          <a class="nav-link" href="index.html" style="color:#101D6B">Home<span class="sr-only">(current)</span></a>
+                          <a class="nav-link" href="index.php" style="color:#101D6B">Home<span class="sr-only">(current)</span></a>
                         </li>
                         <li class="nav-item dropdown">
                           <a class="nav-link dropdown-toggle" href="#" id="complaintsDropdown" role="button"
@@ -103,21 +103,21 @@ function renderNavBar()
                           </a>
                           <div class="dropdown-menu" aria-labelledby="complaintsDropdown">
                             <a class="dropdown-item" href="record-complaints.php" style="color:#101D6B"><b>Add/View Complaints</b></a>
-                            <a class="dropdown-item" href="view-people.html" style="color:#101D6B"><b>View People and Cases</b></a>
-                            <a class="dropdown-item" href="complaint-study.html" style="color:#101D6B"><b>Complaint Study</b></a>
+                            <a class="dropdown-item" href="view-people.php" style="color:#101D6B"><b>View People and Cases</b></a>
+                            <a class="dropdown-item" href="complaint-study.php" style="color:#101D6B"><b>Complaint Study</b></a>
                           </div>
                         </li>
                         <li class="nav-item dropdown">
                           <a class="nav-link dropdown-toggle" href="#" id="dutiesDropdown" role="button"
                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color:#101D6B">Duties</a>
                           <div class="dropdown-menu" aria-labelledby="dutiesDropdown">
-                            <a class="dropdown-item" href="general_Duty_Update.html" style="color:#101D6B"><b>General Duty</b></a>
-                            <a class="dropdown-item" href="special-duty.html" style="color:#101D6B"><b>Special Duty</b></a>
-                            <a class="dropdown-item" href="emergency-duties.html" style="color:#101D6B"><b>Emergency Duty</b></a>
+                            <a class="dropdown-item" href="general-duty.php" style="color:#101D6B"><b>General Duty</b></a>
+                            <a class="dropdown-item" href="special-duty.php" style="color:#101D6B"><b>Special Duty</b></a>
+                            <a class="dropdown-item" href="emergency-duties.php" style="color:#101D6B"><b>Emergency Duty</b></a>
                           </div>
                         </li>
                         <li class="nav-item">
-                          <a class="nav-link" href="payroll.html" style="color:#101D6B">Salary</a>
+                          <a class="nav-link" href="payroll.php" style="color:#101D6B">Salary</a>
                         </li>
                         <li class="nav-item">
                           <a class="nav-link" href="#" style="color:#101D6B;">Settings</a>
@@ -136,7 +136,7 @@ function renderNavBar()
         <div class="row">
           <div class="col-md-12">
               <nav class="navbar navbar-expand-lg navbar-light fixed-top bg-white m-0 mt-0 mb-0">
-                <a class="navbar-brand" href="index.html">
+                <a class="navbar-brand" href="index.php">
                   <div class="d-flex align-items-center">
                     <img src="../assets/logo.png" width="70" height="70" alt="logo">
                     <h3 class="d-none d-lg-inline-block align-top mt-3 h3" style="color:#101D6B">Sri Lanka Police</h3>
@@ -150,7 +150,7 @@ function renderNavBar()
                 <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
                   <ul class="navbar-nav">
                     <li class="nav-item">
-                      <a class="nav-link" href="index.html" style="color:#101D6B">Home<span class="sr-only">(current)</span></a>
+                      <a class="nav-link" href="index.php" style="color:#101D6B">Home<span class="sr-only">(current)</span></a>
                     </li>
                     <form class="form-inline my-2 my-lg-0"  action="loginForm.php" method="get">
                       <button class="btn btn btn-primary my-2 my-sm-0" type="submit" style="color:white;background-color: #101D6B;">Log In</button>
@@ -162,5 +162,3 @@ function renderNavBar()
       </div>';
     }
 }
-
-?>
