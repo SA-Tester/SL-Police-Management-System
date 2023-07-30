97a3c1a08a2ae244f03e284b585dfc9b570a0e3f
Bodhika-Git <105534658+Bodhika-Git@users.noreply.github.com>
2023-07-29 18:19:00 -0700

    Replace mysqli with PDO

--- src/fetch_data_general_duty_table.php
+++ src/fetch_data_general_duty_table.php
@@ -1,50 +1,31 @@
 <?php
-// Replace these database credentials with your actual values
-$servername = 'localhost';
-$username = 'Testuser';
-$password = 'Testuser';
-$dbname = 'sl-police';
+// Include the DbConnector class
+require_once 'DbConnector.php';
 
-// Create connection to the database
-$conn = new mysqli($servername, $username, $password, $dbname);
+try {
+    // Create a new instance of the DbConnector class to establish the database connection
+    $dbConnector = new DbConnector();
 
-// Check connection
-if ($conn->connect_error) {
-    die('Connection failed: ' . $conn->connect_error);
-}
+    // Get the PDO connection object from the DbConnector class
+    $conn = $dbConnector->conn;
 
-// SQL query to fetch data from the table for duty_type = "General"
-$sql = "SELECT empID, duty_type,duty_cause, start, end, location_id FROM duty WHERE duty_type = 'General'";
+    // SQL query to fetch data from the table for duty_type = "General"
+    $sql = "SELECT empID, duty_type, duty_cause, start, end, location_id FROM duty WHERE duty_type = 'General'";
 
-// Execute the query and get the result set
-$result = $conn->query($sql);
+    // Prepare the query and execute it
+    $stmt = $conn->query($sql);
 
-// Check if there are any rows returned
-if ($result->num_rows > 0) {
-    // Initialize an array to hold the fetched data
-    $data = array();
+    // Fetch all rows as an associative array
+    $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    // Loop through the result set and store each row in the array
-    while ($row = $result->fetch_assoc()) {
-        $data[] = $row;
-    }
-
-    // Close the result set
-    $result->close();
-
-    // Close the database connection
-    $conn->close();
+    // Close the database connection (PDO automatically closes the connection when the object is destroyed)
+    $conn = null;
 
     // Convert the array to JSON format and send it as the response
     header('Content-Type: application/json');
     echo json_encode($data);
-} else {
-    // No "General" duties found in the table
-    // Close the database connection
-    $conn->close();
-
-    // Return an empty array as the response
-    header('Content-Type: application/json');
-    echo json_encode(array());
+} catch (PDOException $e) {
+    // If there is an error, die and display the error message
+    die('Connection failed: ' . $e->getMessage());
 }
 ?>
