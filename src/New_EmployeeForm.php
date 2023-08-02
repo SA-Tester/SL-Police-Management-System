8f9c6fd51e5ca63b5ffd77afafa839a09361c505
Gimhani2000 <86472955+Gimhani2000@users.noreply.github.com>
2023-08-02 16:09:36 +0530

    create new employee form

---
+++ src/New_EmployeeForm.php
@@ -0,0 +1,33 @@
+<?php
+
+use classes\employee;
+
+require_once 'classes/employee.php';
+
+
+
+if (isset($_POST['emplD'], $_POST['nic'], $_POST['first_name'], $_POST['last_name'], $_POST['dob'], $_POST['gender'], $_POST['tel_no'], $_POST['email'], $_POST['address'], $_POST['marital_status'], $_POST['rank'], $_POST['appointment_date'], $_POST['retired_status'])) {
+
+    $emplD = strip_tags($_POST['emplD']);
+    $nic = strip_tags($_POST['nic']);
+    $first_name = strip_tags($_POST['first_name']);
+    $last_name = strip_tags($_POST['last_name']);
+    $dob = strip_tags($_POST['dob']);
+    $gender = strip_tags($_POST['gender']);
+    $tel_no = strip_tags($_POST['tel_no']);
+    $email = strip_tags($_POST['email']);
+    $address = strip_tags($_POST['address']);
+    $marital_status = strip_tags($_POST['marital_status']);
+    $rank = strip_tags($_POST['rank']);
+    $appointment_date = strip_tags($_POST['appointment_date']);
+    $retired_status = strip_tags($_POST['retired_status']);
+
+    $register = new employee($emplD, $nic, $first_name, $last_name, $dob, $gender, $tel_no, $email, $address, $marital_status, $rank, $appointment_date, $retired_status);
+    $register->register();
+}
+
+
+
+
+
+
